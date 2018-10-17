50f09739f1046344175ed51aaa081632bed4de84
beery <me@bjoernar.com>
2018-10-17 12:31:36 +0200

    New version

---
+++ precache-manifest.7bf71b92b169ed8529ec07af580fe26f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3de8980b0984f20457be",
+    "url": "/static/js/main.3de8980b.chunk.js"
+  },
+  {
+    "revision": "b1314b0f84aa3f3d75e2",
+    "url": "/static/js/1.b1314b0f.chunk.js"
+  },
+  {
+    "revision": "3de8980b0984f20457be",
+    "url": "/static/css/main.f9219222.chunk.css"
+  },
+  {
+    "revision": "b1314b0f84aa3f3d75e2",
+    "url": "/static/css/1.e79f8d73.chunk.css"
+  },
+  {
+    "revision": "c1fb79e455818d205b4547146e0e0b09",
+    "url": "/index.html"
+  }
+];
