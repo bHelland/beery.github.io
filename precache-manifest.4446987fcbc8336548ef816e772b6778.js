77137d018d71e7778747e3c66ec935e1cdb64b39
beery <me@bjoernar.com>
2018-10-19 14:21:47 +0200

    Updates

---
+++ precache-manifest.4446987fcbc8336548ef816e772b6778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "222ecfb9d92462bd63f0",
+    "url": "/static/js/main.222ecfb9.chunk.js"
+  },
+  {
+    "revision": "89269792919804e97512",
+    "url": "/static/js/1.89269792.chunk.js"
+  },
+  {
+    "revision": "222ecfb9d92462bd63f0",
+    "url": "/static/css/main.e0780b51.chunk.css"
+  },
+  {
+    "revision": "89269792919804e97512",
+    "url": "/static/css/1.f40015f3.chunk.css"
+  },
+  {
+    "revision": "5eaa1dc594eaa1e9733c9c436e5fd609",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b2e072a0702f27152daad27c4d50fd1.js"
+  "/precache-manifest.4446987fcbc8336548ef816e772b6778.js"
 );
 
 workbox.clientsClaim();
